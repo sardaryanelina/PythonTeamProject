1357ee8e13a5475ec91fa213965b9e6aec3557ad
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-10-17 02:32:05 -0400

    3. Project Templates

---
+++ 3.ProjectTemplates/pydjango/django-project/teamwork/django_app/views.py
@@ -0,0 +1,8 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def djangoapp(request):
+    # return HttpResponse("Welcome to Django App Page")
+    return render(request, 'project.html', {})
